a333a9a322b29acf7a5690515959df7b580f2b94
Karla Alcántara García <kalcantara@casystem.com.mx>
2023-03-17 18:22:11 -0600

    prueba de comandos

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -2,8 +2,8 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', (null) => {
-  beforeEach(async (null) => {
+/*describe('AppComponent', () => {
+  beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
@@ -11,24 +11,25 @@ describe('AppComponent', (null) => {
       declarations: [
         AppComponent
       ],
-    }).compileComponents(null);
-  });
+    }).compileComponents();
+  });*/
 
-  it('should create the app', (null) => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frontend'`, (null) => {
+  it(`should have as title 'frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('frontend');
   });
 
-  it('suma 2+2 = 4', (null) => {
+  it('suma 2+2 = 4', () => {
     const suma = 2+2;
     expect(suma).toBe(4);
   });
+
 
 });
